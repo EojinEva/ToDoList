6b31a602890a90129abe3e9e1d175d9eab8000f7
EojinEva <w-w-w-@naver.com>
2023-12-21 09:34:52 +0900

    오류 파티 first to do list

---
+++ ToDoList/Tasks.swift
@@ -0,0 +1,29 @@
+//
+//  Tasks.swift
+//  ToDoList
+//
+//  Created by t2023-m0099 on 12/19/23.
+//
+
+import UIKit
+
+class Tasks: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoList/task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  ToDoList
+//
+//  Created by t2023-m0099 on 12/20/23.
+//
+
+import Foundation
